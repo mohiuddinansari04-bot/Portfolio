16383ef7fbee965ecc851482c8dbf93647226c4d
Mohiuddin ansari <mohiuddinansari04@gmail.com>
2026-06-12 01:02:52 +0530

    ..

--- Portfolio/src/omponent/Header.jsx
+++ Portfolio/src/omponent/Header.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 function Header() {
   return (
-      <nav>
-        <h1>My Portfolio</h1>
-        <div></div>
+      <nav className="bg-white shadow-lg p-4 flex items-center justify-between">
+        <h1 className="text-2xl font-bold text-gray-800" >Ahmed Mohiuddin Ansari</h1>
+        <div>
+
+        </div>
       </nav>
   )
 }
