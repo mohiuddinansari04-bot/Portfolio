d50c1e3483836e0e6dee9ddde2d2aa531599a3f9
Mohiuddin ansari <mohiuddinansari04@gmail.com>
2026-06-12 17:54:53 +0530

    commit

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,23 +1,23 @@
 import './App.css'
+import { useState } from 'react'
 import Header from './Component/Header'
 import Cards from './Component/Project/Cards'
 
 function App() {
+
   return (
     <>
       <div className="min-h-screen bg-gray-600">
         <Header />
         <Cards />
-        
-          {/* frontend */}
-        <div >
-        <main>
-          <h1>My Portfolio</h1>
-        </main>
+
+        <div className="mx-auto max-w-6xl px-4 py-10 sm:px-6 lg:px-8">
+          <main>
+            <h1>My Portfolio</h1>
+            </main>
+        </div>
       </div>
 
-      </div>
-      
     </>
   )
 }
