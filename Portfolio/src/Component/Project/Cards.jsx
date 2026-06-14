a21c6bf6d05f7ea92cc8c870daf21f54fa842153
Mohiuddin ansari <mohiuddinansari04@gmail.com>
2026-06-14 21:40:52 +0530

    commit

--- Portfolio/src/Component/Project/Cards.jsx
+++ Portfolio/src/Component/Project/Cards.jsx
@@ -23,15 +23,14 @@ function Cards() {
     },
   ]
   return (
-    <div className="mx-auto max-w-6xl px-4 py-10 sm:px-6 lg:px-8">
-      <h1 className="text-3xl font-bold mb-8">My Projects</h1>
-      <p className="text-2xl w-3xl ">Here are some of my recent projects created by me using React and modern web technologies:</p>
+    <div id="projects" className="mx-auto max-w-6xl px-4 py-16 sm:px-6 lg:px-8">
+      <h1 className="text-3xl font-bold mb-8 text-center">My Projects</h1>
+      <p className="text-2xl mx-auto max-w-3xl text-center">Here are some of my recent projects created by me using React and modern web technologies:</p>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-10 justify-items-center">
         {cardsData.map((card) => (
-          <div key={card.id} className="w-full max-w-sm bg-gray-500 rounded-lg shadow-lg  overflow-hidden text-center p-4">
-     
-            <h3 className=" text-2xl font-semibold text-white ">{card.title}</h3>
+          <div key={card.id} className="w-full max-w-sm bg-gray-500 rounded-lg shadow-lg overflow-hidden text-center p-6">
+            <h3 className="text-2xl font-semibold text-white">{card.title}</h3>
             <p className="mt-5 text-sm text-white">{card.description}</p>
             <a href={card.link} target="_blank" rel="noopener noreferrer" className="mt-4 inline-block px-4 py-2 bg-yellow-500 text-white rounded hover:bg-yellow-600 transition">
               View Project
