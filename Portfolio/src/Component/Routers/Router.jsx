2cddd5ae5e306c2d0962d4bdc39a33a620ad08db
Mohiuddin ansari <mohiuddinansari04@gmail.com>
2026-06-15 12:51:21 +0530

    commit

--- Portfolio/src/Component/Routers/Router.jsx
+++ Portfolio/src/Component/Routers/Router.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Layout from '../Layout'
 import Project from '../ProjectPages/Project'
+import {  Routes, Route } from 'react-router-dom'
 
 function AppRouter() {
   return (
-    <div>Router</div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="project" element={<Project />} />
+      </Route>
+    </Routes>
   )
 }
 
